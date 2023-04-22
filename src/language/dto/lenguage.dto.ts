2ad4879a68afb40dfbae8cf5696b9f5374da0488
Federico Eloy Fucci <federicofucci@hotmail.com>
2023-04-21 22:43:54 -0300

    lenguageDTO (#6)

---
+++ src/language/dto/lenguage.dto.ts
@@ -0,0 +1,22 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { LEVELS } from '../../constants/enums/levels';
+
+export class LenguageDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(LEVELS)
+  level: LEVELS;
+}
+
+export class LenguageUpdateDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsEnum(LEVELS)
+  level: LEVELS;
+}
