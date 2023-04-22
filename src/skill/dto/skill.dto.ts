5aabf8ac825ae4d306b74e21a104d530fa923b8e
Federico Eloy Fucci <federicofucci@hotmail.com>
2023-04-21 22:19:20 -0300

    skill/skillDTO (#5)
    * skill/skillDTO
    
    * skillUpdateDTO

---
+++ src/skill/dto/skill.dto.ts
@@ -0,0 +1,22 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { LEVELS } from '../../constants/enums/levels';
+
+export class SkillDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(LEVELS)
+  level: LEVELS;
+}
+
+export class SkillUpdateDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsEnum(LEVELS)
+  level: LEVELS;
+}
