ca02571aeebed0d8d1c3c30255cb010788a02764
federicofucci21 <federicofucci@hotmail.com>
2023-04-21 22:15:18 -0300

    skillUpdateDTO

--- src/skill/dto/skill.dto.ts
+++ src/skill/dto/skill.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { LEVELS } from '../../constants/enums/levels';
 
 export class SkillDTO {
@@ -7,6 +7,16 @@ export class SkillDTO {
   name: string;
 
   @IsNotEmpty()
+  @IsEnum(LEVELS)
+  level: LEVELS;
+}
+
+export class SkillUpdateDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
   @IsEnum(LEVELS)
   level: LEVELS;
 }
