f8f9c7e5464e1004de08016959c9e4be3a9c91af
federicofucci21 <federicofucci@hotmail.com>
2023-05-08 20:25:00 -0300

    fix import

--- src/user/profile/profile.service.ts
+++ src/user/profile/profile.service.ts
@@ -2,6 +2,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProfileEntity } from './entities/profile.entity';
 import { ProfileDTO } from './dto/profile.dto';
+import { HttpException } from '@nestjs/common';
 
 export class ProfileService {
   constructor(
@@ -15,6 +16,23 @@ export class ProfileService {
       if (!profile) {
         return 'bad response';
       }
+      return profile;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async findOneByEmail(email: string): Promise<ProfileEntity> {
+    try {
+      const profile: ProfileEntity = await this.profileRepository
+        .createQueryBuilder('profile')
+        .where({ email })
+        .getOne();
+
+      if (!profile) {
+        throw new HttpException('Profile Not Found', 404);
+      }
+
       return profile;
     } catch (error) {
       console.log(error);
