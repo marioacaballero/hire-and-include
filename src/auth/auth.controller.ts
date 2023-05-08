073a1beee2870f491272a8d364d634cfc20466c3
federicofucci21 <federicofucci@hotmail.com>
2023-05-08 20:19:06 -0300

    login JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ProfileDTO } from '../user/profile/dto/profile.dto';
-// import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   //Register User(Profile)
-  @Post()
+  @Post('register')
   registerUser(@Body() userObject: ProfileDTO) {
     return this.authService.register(userObject);
   }
 
-  //Login User
-  // @Post()
+  // Login User
+  @Post('login')
+  loginUser(@Body() userObject: ProfileDTO) {
+    return this.authService.login(userObject);
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ProfileEntity } from '../user/profile/entities/profile.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../constants/jwtConstants';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProfileEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProfileEntity]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '20h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
