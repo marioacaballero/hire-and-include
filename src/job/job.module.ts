41df1d17a799779961e3fe52f489cd58eec48127
Mario Caballero <mario.caballero.948@gmail.com>
2023-05-08 00:01:16 -0300

    fix job module

--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { SeniorityModule } from './complements/seniority/seniority.module';
 
 @Module({
-  imports: [SeniorityModule]
+  imports: [SeniorityModule],
 })
 export class JobModule {}
