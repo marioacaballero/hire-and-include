4f87bb1032087215a19f8bc99a0af715bf0d684c
Mario Caballero <mario.caballero.948@gmail.com>
2023-05-17 21:03:18 -0300

    fix earUs-

--- src/profile/complements/ear-us/entities/ear-us.entity.ts
+++ src/profile/complements/ear-us/entities/ear-us.entity.ts
@@ -1,6 +1,6 @@
 import { OneToMany } from 'typeorm';
 import { BaseEntity } from '../../../../config/base.entity';
-import { ProfileEntity } from 'src/profile/entities/profile.entity';
+import { ProfileEntity } from '../../../../profile/entities/profile.entity';
 
 export class EarUsEntity extends BaseEntity {
   name: string; //nombre de la red social, etc
