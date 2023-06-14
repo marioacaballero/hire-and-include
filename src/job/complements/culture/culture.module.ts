dc9358a7ac62fee23b4124383817b18ed9e5de8d
Mario Caballero <mario.caballero.948@gmail.com>
2023-06-13 23:59:09 -0300

    add modules

--- src/job/complements/culture/culture.module.ts
+++ src/job/complements/culture/culture.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { CultureService } from './services/culture.service';
 
 @Module({
-  providers: [CultureService]
+  providers: [CultureService],
 })
 export class CultureModule {}
