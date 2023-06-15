9efe5f5e6ec88bf13c69919cfd5fbd043c73b261
Mario Caballero <mario.caballero.948@gmail.com>
2023-06-15 16:30:09 -0300

    add basic controller on profile

--- src/profile/complements/ear-us/ear-us.module.ts
+++ src/profile/complements/ear-us/ear-us.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EarUsService } from './services/ear-us.service';
+import { EarUsController } from './controllers/ear-us.controller';
 
 @Module({
   providers: [EarUsService],
+  controllers: [EarUsController],
 })
 export class EarUsModule {}
