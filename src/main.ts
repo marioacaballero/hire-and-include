0b333ae6656d31f46f2226fcf8986dd86e41f58d
Mario Caballero <mario.caballero.948@gmail.com>
2023-08-25 15:02:39 -0300

    fix: add ValidationPipe to use DTOs and add cors config

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { CORS } from './constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { snapshot: true });
 
   app.use(morgan('dev'));
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  ); //con este global pipe puedo utilizar los dto a traves del ValidationPipe
+
   const reflector = app.get(Reflector); //creo el reflector para evitar mostrar ciertos datos
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector)); //uso el reflector en el interceptor
+
+  app.enableCors(CORS);
 
   app.setGlobalPrefix('api/v1');
 
